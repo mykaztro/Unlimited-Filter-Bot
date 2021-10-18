01cd2d3e455bd704da1e1df107dc7a368f961ff1
mykaztro <87994885+mykaztro@users.noreply.github.com>
2021-10-19 02:39:10 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,56 @@
 class Script(object):
 
-    START_MSG = """<b>Hai {},
+    START_MSG = """<b>Hy {},
 
-ഇവിടെ വന്നിട്ട് യാതൊരു കാര്യവുമില്ല എന്നെ 📽️★[ᴘ®ɪⓂ️ᴇ ©ɪɴᴇⓂ️ᴀꜱ]★📺 ഗ്രൂപ്പിലെ മാത്രമേ ആഡ് ചെയ്യാൻ കഴിയൂ
+I'm an advanced filter bot with many capabilities!
+There is no practical limits for my filtering capacity :)
+
+See <i>/help</i> for commands and more details.</b>
+"""
+
+
+    HELP_MSG = """
+<i>Add me as admin in your group and start filtering :)</i>
+
+
+<b>Basic Commands;</b>
+
+/start - Check if I'm alive!
+/help - Command help
+/about - Something about me!
+
+
+<b>Filter Commands;</b>
+
+<code>/add name reply</code>  -  Add filter for name
+
+<code>/del name</code>  -  Delete filter
+
+<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
+
+<code>/viewfilters</code>  -  List all filters in chat
+
+
+<b>Connection Commands;</b>
+
+<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
+<code>/connect</code> in groups.
+
+<code>/connections</code>  -  Manage your connections.
+
+
+<b>Extras;</b>
+
+/status  -  Shows current status of your bot (Auth User Only)
+
+/id  -  Shows ID information
+
+<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
+
+
+<b>© @TroJanzHEX</b>
+"""
+
 
     ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
 
