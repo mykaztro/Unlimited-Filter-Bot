8f5f9b61c04839419a0ebc0494853856f627f6df
mykaztro <87994885+mykaztro@users.noreply.github.com>
2021-10-19 02:43:04 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,6 +5,8 @@ class Script(object):
 I'm an advanced filter bot with many capabilities!
 There is no practical limits for my filtering capacity :)
 
+See <i>/help</i> for commands and more details.</b>
+"""
 
 
     HELP_MSG = """
