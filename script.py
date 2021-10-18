985deafff0e7d275bbd3ad27b9ab781c450acdb1
mykaztro <87994885+mykaztro@users.noreply.github.com>
2021-10-19 02:50:13 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
 class Script(object):
 
     START_MSG = """<b>Hy {},
-ഞാൻ ഒരു ആടാർ ഫിൽറ്റർ ബോട്ട് ആണ്, എൻറെ വിധി എൻറെ [അഡ്മിന്റെ](https://t.me/Myfreak123) തീരുമാനങ്ങളാണ്.
+ഞാൻ ഒരു ആടാർ ഫിൽറ്റർ ബോട്ട് ആണ്, എൻറെ വിധി എൻറെ [അഡ്മിന്റെ](https://t.me/Sanoob_Achu_18) തീരുമാനങ്ങളാണ്.
 """
 
 
     HELP_MSG = """<b>Go Away Stupit In The House And My Daughter എഴിച്ചു പോടേയ് .</b>"""
 
 
-    ABOUT_MSG = """നിങ്ങൾക് [donate](https://t.me/Myfreak123) താല്പര്യം ഉണ്ടെകിൽ എന്റെ അഡ്മിനുമായി സംസാരിക്കുക."""
+    ABOUT_MSG = """നിങ്ങൾക് [donate](https://t.me/Sanoob_Achu_18) താല്പര്യം ഉണ്ടെകിൽ എന്റെ അഡ്മിനുമായി സംസാരിക്കുക."""
