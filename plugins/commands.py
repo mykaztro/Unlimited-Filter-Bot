1f440e5208fe62fda03ab297564be11e5fb61db8
mykaztro <87994885+mykaztro@users.noreply.github.com>
2021-10-19 02:59:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -214,10 +214,11 @@ async def start(client, message):
     await message.reply_text(
         text=Script.START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
+        
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Command Help", callback_data="help_data")
+                    InlineKeyboardButton("🔰GROUP🔰", url="https://t.me/Movies_Club_2019")
                 ]
             ]
         ),
@@ -243,12 +244,10 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
+                InlineKeyboardButton("CLOSE ⚡️", callback_data="close_data"),
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    
                 ]
             ]
         ),
@@ -264,12 +263,11 @@ async def about(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                    
                 ],
                 [
-                    InlineKeyboardButton("BACK", callback_data="help_data"),
-                    InlineKeyboardButton("CLOSE", callback_data="close_data"),
+                    
+                    InlineKeyboardButton("CLOSE ⚡️", callback_data="close_data"),
                 ]                
             ]
         ),
